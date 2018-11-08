36b9fdfcd5b823552e6f9e86982d0888d6886052
Hanumant Singh <hanumant07@gmail.com>
2018-11-07 16:46:32 -0800

    cleanup utils file
    Signed-off-by: Hanumant Singh <hanumant07@gmail.com>

--- utils.py
+++ utils.py
@@ -1,89 +1,131 @@
+from collections import defaultdict
 import csv
 import os
-from collections import defaultdict
+import numpy as np
 
-_writeup_dir = './writeup_dir'
+_WRITEUP_DIR = './writeup_dir'
 
-'''
-save_fig_for_writeup: Function to save a matplot lib figure as image file
 
-    @fig : matplot lib fig
-    @ fname : base file name of image file
-'''
 def save_fig_for_writeup(fig, fname):
-    assert(fig is not None)
-    file_path = os.path.join(_writeup_dir, fname)
-    fig.savefig(file_path)
+  """Function to save a matplot lib figure as image file.
+
+  Args:
+    fig: matplot lib fig instance.
+    fname: base file name of image file.
+
+  Raises:
+    ValueError: if figname or fname are not valid entries.
+  """
+
+  if fig is None:
+    raise ValueError('Pass valid matplot lib fig')
+  if fname is None:
+    raise ValueError('Pass valid filename')
+  file_path = os.path.join(_WRITEUP_DIR, fname)
+  fig.savefig(file_path)
 
 
-'''
-TrafficDataInfo: Class to parse and analyze the traffic sign data
-'''
-class TrafficDataInfo():
+class TrafficDataInfo(object):
+  """Parse and analyze the traffic sign data."""
 
-    ''' Parse csv file containing class id to sign names'''
-    def _populate_ids_to_names(self, names_files):
-        with open('signnames.csv', mode='r') as csv_file:
-            signs = csv.DictReader(csv_file)
-            for row in signs:
-                self._ids_to_names[int(row['ClassId'])] = row['SignName']
+  def _populate_ids_to_names(self, names_files):
+    """Parse csv file containing class id to sign names.
 
+    Args:
+      names_files: csv file name.
 
-    '''
-    @names_file: Csv file that contains a mapping to traffic sign names
-    '''
-    def __init__(self, names_file):
-        self._ids_to_names = {}
-        self._populate_ids_to_names(names_file)
+    Raises:
+      ValueError if names_files is not a str
+    """
+    if not isinstance(names_files, str):
+      raise ValueError('names_files needs to be valid string')
+    with open('signnames.csv', mode='r') as csv_file:
+      signs = csv.DictReader(csv_file)
+      for row in signs:
+        self._ids_to_names[int(row['ClassId'])] = row['SignName']
 
+  def __init__(self, names_file):
+    """Inits TrafficDataInfo.
 
-    '''
-    organize_data_by_label: Create a mapping from class id to sample index from dataset
-    @labels: A list of class ids. It is assumed entries in the list correspond to same position
-             of the corresponding sample in the dataset
-    '''
-    def organize_data_by_label(self, labels):
-        label_to_data = defaultdict(list)
-        for i in range(len(labels)):
-            label_to_data[labels[i]].append(i)
-        return label_to_data
+    Args:
+      names_file: Csv file that contains a mapping to traffic sign names
+    """
+    self._ids_to_names = {}
+    self._populate_ids_to_names(names_file)
 
+  def organize_data_by_label(self, labels):
+    """Create a mapping from class id to sample index from dataset.
 
-    '''
-    get_name_for_label: Map label id to sign name
-    @label_id: The id that represents the sign as per the input csv file
-    '''
-    def get_name_for_label(self, label_id):
-        return self._ids_to_names[label_id]
+    Args:
+      labels: A list of class ids. It is assumed entries in the list correspond
+        to same position of the corresponding sample in the dataset.
 
+    Returns:
+      A dictionray that maps label id to sample index.
 
-    '''
-    get_hist_info: Get histogram information for the datasamples
-    @labels: The labels is a list of class ids corresponding to the samples in the data set.
-    The histogram information pertains to the number of samples for a given class
-    '''
-    def get_hist_info(self, labels):
-        return np.unique(labels, return_counts=True)
+    Raises:
+      ValueError: if labels is not a list.
+    """
+    if not isinstance(labels, list):
+      raise ValueError('labels should be a non empty list')
+    label_to_data = defaultdict(list)
+    for i in range(len(labels)):
+      label_to_data[labels[i]].append(i)
+    return label_to_data
 
+  def get_name_for_label(self, label_id):
+    """Map label id to sign name.
 
-    '''
-    add_existing_samples: Add new samples to the existing training set
-    @training_x: Existing training set
-    @training_y: Existing training labels
-    @new_x: New samples to add
-    @new_y: Corresponding new labels to add
-    '''
-    def add_existing_samples(self, training_x, training_y, new_x, new_y):
-        labels, label_counts = self.get_hist_info(training_y)
-        new_data_by_label = self.organize_data_by_label(new_y)
-        max_count = label_counts.max()
+    Args:
+      label_id: The id that represents the sign as per the input csv file.
 
-        for i, label in enumerate(labels):
-            if (label_counts[i] > (max_count / 2)):
-                continue
-            valid_indices = new_data_by_label[label]
-            new_samples = new_x[np.array(valid_indices)]
-            new_labels = np.repeat(label, len(new_samples))
-            training_x = np.concatenate((training_x, new_samples), axis=0)
-            training_y = np.concatenate((training_y, new_labels), axis=0)
-        return training_x, training_y
+    Returns:
+      sign name for given id.
+
+    Raises:
+      ValueError if label_id is None.
+    """
+    if not isinstance(label_id, int):
+      raise ValueError('label_id should be a valid int')
+    return self._ids_to_names[label_id]
+
+  def get_hist_info(self, labels):
+    """Get histogram information for the datasamples.
+
+    The histogram information pertains to the number of samples for a given
+    class.
+
+    Args:
+      labels: The labels is a list of class ids corresponding to the samples in
+        the data set.
+
+    Returns:
+      sorted list of class ids and their respective sample counts.
+    """
+    return np.unique(labels, return_counts=True)
+
+  def add_existing_samples(self, training_x, training_y, new_x, new_y):
+    """Add new samples to the existing training set.
+
+    Args:
+      training_x: Existing training set.
+      training_y: Existing training labels.
+      new_x: New samples to add.
+      new_y: Corresponding new labels to add.
+
+    Returns:
+      new training samples.
+    """
+    labels, label_counts = self.get_hist_info(training_y)
+    new_data_by_label = self.organize_data_by_label(new_y)
+    max_count = label_counts.max()
+
+    for i, label in enumerate(labels):
+      if label_counts[i] > (max_count / 2):
+        continue
+      valid_indices = new_data_by_label[label]
+      new_samples = new_x[np.array(valid_indices)]
+      new_labels = np.repeat(label, len(new_samples))
+      training_x = np.concatenate((training_x, new_samples), axis=0)
+      training_y = np.concatenate((training_y, new_labels), axis=0)
+    return training_x, training_y
